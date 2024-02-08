9eccd1c782c694d33597cbf023788a10be300831
Eda OZDEMIR <edaozdemir@live.fr>
2024-02-08 16:50:23 +0100

    refactoring model.js

--- model.js
+++ model.js
@@ -2,6 +2,10 @@ import { displayTenStories, displayError } from "./main.js";
 
 //MODEL
 
+function handleError(error) {
+  console.error(error);
+  displayError();
+}
 
 //récupération des ids des NOUVELLES histoires
 async function fetchStoriesIds() {
@@ -17,10 +21,9 @@ async function fetchStoriesIds() {
     const ids = response.json();
     return ids;
   } catch (error) {
-    console.error(error);
+    handleError(error);
   }
 }
-
 
 //récupération d'un id spécifique d'une histoire
 async function fetchStory(id) {
@@ -37,36 +40,33 @@ async function fetchStory(id) {
 
     return story;
   } catch (error) {
-    console.error(error);
+    handleError(error);
   }
 }
 
-
 //je veux récupérer 10 histoires , je fetch tous les id
 export async function fetchTenStories(index) {
-    try {
-      //on recupère les 10 premieres nouvelles histoires
-      const ids = await fetchStoriesIds();
-  
-      // Calcul du début et de la fin de la plage d'indices
-      //si index = 1, alors on commence à ajouter +10 histoires à partir de 10
-      const start = index * 10;
-      const end = start + 10;
-  
-      // Extraction des 10 identifiants à partir du tableau
-      const slicedIds = ids.slice(start, end);
-  
-      // Récupération des histoires pour les 10 identifiants, on attend qu'elles soient toutes récupérées pour les afficher
-      const tenStories = await Promise.all(slicedIds.map((id) => fetchStory(id)));
-  
-      // Affichage des histoires
-      displayTenStories(tenStories);
-  
-      return tenStories;
-    } catch (error) {
-      // Affichage de l'erreur
-      displayError();
-      console.error(error);
-    }
+  try {
+    //on recupère les 10 premieres nouvelles histoires
+    const ids = await fetchStoriesIds();
+
+    // Calcul du début et de la fin de la plage d'indices
+    //si index = 1, alors on commence à ajouter +10 histoires à partir de 10
+    const start = index * 10;
+    const end = start + 10;
+
+    // Extraction des 10 identifiants à partir du tableau
+    const slicedIds = ids.slice(start, end);
+
+    // Récupération des histoires pour les 10 identifiants, on attend qu'elles soient toutes récupérées pour les afficher
+    const tenStories = await Promise.all(slicedIds.map((id) => fetchStory(id)));
+
+    // Affichage des histoires
+    displayTenStories(tenStories);
+
+    return tenStories;
+  } catch (error) {
+    // Affichage de l'erreur
+    handleError(error);
   }
-  
+}
