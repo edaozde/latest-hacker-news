8a0e700984bc03c2d43985e778dcc12400a47750
Eda OZDEMIR <edaozdemir@live.fr>
2024-02-12 14:19:07 +0100

    first test cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+//tester l'url
+
+describe("My First Test", () => {
+  it("Visits hacker-news", () => {
+    cy.visit("https://latest-hacker-news.vercel.app/");
+  });
+});
