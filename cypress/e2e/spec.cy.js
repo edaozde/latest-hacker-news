2688224a4a4a2e79a9db460376579da721059199
Eda OZDEMIR <edaozdemir@live.fr>
2024-02-12 17:28:04 +0100

    first cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,16 @@
-//tester l'url
+describe('Hacker News App', () => {
+  beforeEach(() => {
+    cy.visit('https://latest-hacker-news.vercel.app/');
+  });
 
-describe("My First Test", () => {
-  it("Visits hacker-news", () => {
-    cy.visit("https://latest-hacker-news.vercel.app/");
+  it('should load 10 more stories when "Load More" button is clicked', () => {
+    cy.get('.story').should('have.length', 10);
+
+    cy.contains('Load More').click();
+
+    cy.get('.story').should('have.length', 20);
+
+
   });
 });
+
